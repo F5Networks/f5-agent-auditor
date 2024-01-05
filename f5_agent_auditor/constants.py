13da642d4d5cff006b1ef3b3c9440016117aaf3f
pzhang <peter.zhang@f5.com>
2024-01-05 08:22:17 +0000

    check diff vlan,

---
+++ f5_agent_auditor/constants.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# we donot check selfip, route_domain, vlan, because these resources
+# are audited by Neutron DB.
+
+EXCLUSIVE_RESOURCES = [
+    "selfip", "pool", "persistence", "member", "route_domain",
+    "vlan", "folder",
+
+    "sys", "device", "hash_persistence", "websocket_profile",
+    "tunnel", "arp", "msrdp_persistence",
+    "ssl_persistence", "oneconnect"
+]
+
+# checking diff of shared network resource
+# selfip (tenant id, subnet id), is always different because of the hostname.
+# vlan use description to trace
+SHARED_NET_RESOURCES = [
+    "vlan", "route_domain",
+]
+
+# checking diff of loadbalancer (lb id)
+LOADBALANCER_RESOURCES = [
+    "virtual_address", "snatpool",
+]
+
+# checking diff of listener (ls id?)
+# ssl_cert_file most attr is mutable
+# server_ssl_profile is not used
+LISTENER_RESOURCES = [
+    "cookie_persistence", "universal_persistence",
+    "sip_persistence", "source_addr_persistence",
+
+    "http_profile", "tcp_profile", "http2_profile",
+
+    "client_ssl_profile",
+    "cipher_group", "cipher_rule",
+
+    "bwc_policy", "l7policy"
+]
+
+# checking diff of monitor (monitor id)
+MONITOR_RESOURCES = [
+    "http_monitor", "https_monitor", "tcp_monitor", "ping_monitor"
+]
+
+SPEC_RESOURCES = [
+    "selfip", "pool", "member"
+]
+
+EXCLUSIVE_KEYS = [
+    "ifIndex", "generation", "creationTime",
+    "lastModifiedTime", "vsIndex", "ifIndex",
+    "selfDevice", "expirationString", "lastUpdateTime",
+    "checksum", "serialNumber", "expirationDate",
+    "fingerprint", "createTime", "vlansReference",
+    "revision", "passphrase", "subject", "lastModified",
+    "members_s", "membersReference", "interfaces_s"
+]
